2cdbb28e2fc7c57f8a5e4cbd4d9c4e2aa66a3357
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-07-17 21:13:18 +0300

    Add project files.

---
+++ Clone.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clones
+{
+    public class Clone
+    {
+        public int ID { get;}
+        private string _currentProgram = string.Empty;
+        private Stack<string> _learnedPrograms = new();
+        private Stack<string> _rollBackPrograms = new();
+
+        public Clone(int id = 1, string program = "basic")
+        {
+            ID = id;
+            _currentProgram = program;
+            _learnedPrograms.Push(program);
+        }
+
+        public Stack<string> GetLearnedPrograms() => _learnedPrograms; 
+
+        public string Check() => _currentProgram;
+
+        public void Learn(string program)
+        {
+            _currentProgram = program;
+            _learnedPrograms.Push(program);
+        }
+
+        public void RollBack()
+        {
+            var rollBackProgram = _learnedPrograms.Pop();
+            _currentProgram = _learnedPrograms.Peek();
+            _rollBackPrograms.Push(rollBackProgram);
+        }
+
+        public void Relearn()
+        {
+            var programToLearn = _rollBackPrograms.Pop();
+            _currentProgram = _learnedPrograms.Peek();
+            _learnedPrograms.Push(programToLearn);
+        }
+    }
+}
---
+++ CloneVersionSystem.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Clones;
+
+public class CloneVersionSystem : ICloneVersionSystem
+{
+	private List<Clone> _armyClones = new();
+
+	public string Execute(string query)
+	{
+		string command = query.Split(' ')[0];
+		string receiver = query.Split(' ')[1];
+		string program = query.Split(' ')[2];
+
+	}
+}
